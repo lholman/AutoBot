337223b950ea17d5c28bb3887a322a22c4ae326d
Lloyd Holman <lloyd.holman@datumgenerics.net>
2011-11-08 23:48:59 +0000

    Initial commit of basic command line app to dynamically execute Powershell modules

---
+++ WinBot.Cmd/CommandLine.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+
+namespace WinBot.Cmd
+{
+    public class CommandLine
+    {
+        public Command GetCommand { get; private set; }
+
+        public CommandLine(string[] args)
+        {
+            GetCommand = args.Count() == 1 ? new Command(args[0], string.Empty) : new Command(args[0], args[1]);
+        }
+
+        public enum ExitCode : int
+        {
+            Success = 0,
+            CommandLineError = 1,
+            MissingResourceFile = 2,
+            UnknownError = 10
+        }
+
+    }
+
+    public class Command
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        public string CommandText { get; set; }
+        public string ParameterText { get; set; }
+
+        public Command(string scriptName, string parameter)
+        {
+            CommandText = scriptName;
+            ParameterText = parameter;
+        }
+    }
+}
