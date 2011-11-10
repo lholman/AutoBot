d84b246eeffb2e8fea500eeccf84658b93d062b5
Lloyd Holman <lloyd.holman@datumgenerics.net>
2011-11-10 22:25:43 +0000

    Updated AssemblyInfo to support stamping of build version from TeamCity

--- trunk/src/AutoBot.Cmd/Properties/AssemblyInfo.cs
+++ trunk/src/AutoBot.Cmd/Properties/AssemblyInfo.cs
@@ -5,12 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AutoBot.Cmd")]
+[assembly: AssemblyTitle("AutoBot")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("AutoBot.Cmd")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoBot")]
+[assembly: AssemblyCopyright("Copyright © 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,15 +21,12 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bc383ace-32a5-4ed0-adc9-575e7a203090")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+#if DEBUG
+	[assembly: AssemblyInformationalVersion("1.0.0.0_TrunkDeveloperDebug")]
+	[assembly: AssemblyConfiguration("Debug")]
+#else
+	[assembly: AssemblyInformationalVersion("1.0.0.0_TrunkDeveloperRelease")]
+	[assembly: AssemblyConfiguration("Release")]
+#endif
