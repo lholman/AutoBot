5f8ad54b03d94d19bb17aab6a5973e300913cee4
Lloyd Holman <lloyd.holman@datumgenerics.net>
2011-11-10 10:19:56 +0000

    Added app.config.example and added exclusion for App.config

---
+++ trunk/src/AutoBot.UnitTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using AutoBot.Cmd;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoBot.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+          
+        }
+    }
+}
