37e4bc34a3d7f940f4b281a07af21f9b352ca9d6
Lloyd Holman <lloyd.holman@datumgenerics.net>
2011-11-11 22:48:22 +0000

    Improved Get-Help a little, added raw bones for Set-Profile script and aded HipChat.Net NuGet package

--- src/AutoBot.Cmd/Scripts/Get-Help.psm1
+++ src/AutoBot.Cmd/Scripts/Get-Help.psm1
@@ -13,8 +13,12 @@
     Get-Help
 Description
 ------------
-Returns a list of names of all *.psm1 PowerShell Modules in the AutoBot\Scripts folder and optionally gives
-detailed help on a given script
+Returns a list of names of all *.psm1 PowerShell Modules in the AutoBot\Scripts folder
+.EXAMPLE
+    Get-Help Set-Profile
+Description
+------------
+Returns PowerShell native get-help detail for the Set-Profile AutoBot script module
 
 #>
 [cmdletbinding()]
@@ -33,14 +37,18 @@ Process {
 				If ($modulename -ne "")
 				{
 					#Write-Output "$modulename provided"
-					Microsoft.PowerShell.Core\Import-Module ".\$modulename.psm1"
-					$ghelp = Microsoft.PowerShell.Core\Get-Help $moduleName
+					If ($modulename -ne "Get-Help")
+					{
+						Microsoft.PowerShell.Core\Import-Module ".\Scripts\$modulename.psm1"
+						$ghelp = Microsoft.PowerShell.Core\Get-Help $moduleName
+					}
+					
 				}
 				else 
-				{						
-					$dir = Get-ChildItem -recurse
-					$fileList = $dir | Where {$_.extension -eq ".psm1"}
-					$ghelp = $fileList | Format-Table name
+				{
+					$ghelp = "Hi, I'm AutoBot, I have the following scripts installed and ready to run. `r`n"						
+					$ghelp += Get-ChildItem -recurse | Where {$_.extension -eq ".psm1"} | ForEach-Object {(Split-Path $_.name -leaf).ToString().Replace(".psm1", "")}
+					$ghelp += "`r`nFor more information about an installed script try @AutoBot get-help <scriptname> e.g. @AutoBot get-help set-profile. `r`nFind more scripts at https://github.com/lholman/AutoBot-Scripts/tree/master/src/Scripts" 
 				}
 			}
 			catch [Exception] {
